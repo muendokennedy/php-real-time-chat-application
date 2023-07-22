695aeae808c2e1a0ccf494179fae1256dabce6fe
muendokennedy <kennedymunyao999@gmail.com>
2023-07-22 14:13:31 +0300

    Changed the status upadate text

--- logout.php
+++ logout.php
@@ -6,7 +6,7 @@ if(isset($_SESSION["unique_id"])){
 
   require_once "php/config.php";
   // Update the user with offline status once they log out
-  $sql = $conn->query("UPDATE users SET status = 'Offline' WHERE unique_id = {$_GET['user_id']};");
+  $sql = $conn->query("UPDATE users SET status = 'Offline now' WHERE unique_id = {$_GET['user_id']};");
 
   if($sql){
     session_unset();
