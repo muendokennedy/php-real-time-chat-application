d5abc1d01129b9d6c391abe669c06c1f102178f9
muendokennedy <kennedymunyao999@gmail.com>
2023-07-22 14:01:16 +0300

    redirect the user to the users page if already logged in the same browser

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php require_once "header.php"; ?>
+<?php
+ require_once "header.php";
+ 
+ session_start();
+
+if(isset($_SESSION['unique_id'])){
+  header("Location: users.php");
+}
+ ?>
 <body>
   <div class="wrapper">
     <section class="form signup">
